23c7a7d00651cd3bbdeb9e0d0eca76eca18939c9
lazycece <lazycece@gmail.com>
2019-03-13 22:03:11 +0800

    dict

---
+++ admin-dict/src/main/java/com/lazycece/admin/dict/AdminDictApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.admin.dict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminDictApplication.class, args);
+    }
+
+}
